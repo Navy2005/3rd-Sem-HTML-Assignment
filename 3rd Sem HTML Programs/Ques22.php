b8e6676b13e0cab7a482e1d2aa51b59d97648b54
Navy2005 <147182700+Navy2005@users.noreply.github.com>
2024-11-16 00:41:09 +0530

    Add files via upload

---
+++ 3rd Sem HTML Programs/Ques22.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Question 22 </title>
+</head>
+<body>
+    <h1>Server Date And Time</h1>
+    <?php 
+     print "Today is ".date("Y/m/d");
+     print "Time is ".date("h:i:sa");
+    ?>
+</body>
+</html>
